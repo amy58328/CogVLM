77846d45ff1941322911ffcf99d6b83bbdb08160
吳佩臻 <t111598012@ntut.edu.tw>
2024-04-01 14:02:03 +0800

    updata test data

--- python/boundingbox.py
+++ python/boundingbox.py
@@ -1,7 +1,7 @@
 import cv2
 
-# img_path = input("input image")
-img_path = '16.jpg'
+img_path = input("input image")
+# img_path = '14.jpg'
 image = cv2.imread(f'../CogVLM testdata/{img_path}')
 
 h = image.shape[0]
@@ -18,7 +18,7 @@ x2, y2 = int(input_arr[2]), int(input_arr[3])
 
 cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2) 
 
-# image = cv2.resize(image,(w,h))
+image = cv2.resize(image,(w,h))
 
 cv2.imshow('Image with Box', image)
 cv2.waitKey(0)
